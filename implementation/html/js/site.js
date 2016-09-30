f044b2ee4aa90650cee5b4e9a667ad9d667fc229
Gustavo Felix <gustavojofelix@gmail.com>
2016-09-30 15:10:55 +0200

    cimmited from vscode

--- implementation/html/js/site.js
+++ implementation/html/js/site.js
@@ -1,33 +1,14 @@
+
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 6
-    });
-    var infoWindow = new google.maps.InfoWindow({ map: map });
+    var mapOptions = {
+    center: new google.maps.LatLng(51.508742,-0.120850),
+    zoom:9,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-        }, function () {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+  //var map1 = new google.maps.Map(document.getElementById("map-info"),mapOptions);
+  var map2 = new google.maps.Map(document.getElementById("map"),mapOptions);
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-}
+
